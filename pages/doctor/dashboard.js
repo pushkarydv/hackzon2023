bd2e0a4feeb032d178627b3cb05727575ba29a08
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2023-01-06 04:32:30 +0530

    doctor's dashboard

--- pages/doctor/dashboard.js
+++ pages/doctor/dashboard.js
@@ -1,10 +1,10 @@
 import { doc, getDoc } from "firebase/firestore";
 import { useSession } from "next-auth/react";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
-import AuthButton from "../../components/AuthenticationButtons/AuthButton";
 import { db } from "../../config/firebase";
-
+import AuthButton from "../../components/AuthenticationButtons/AuthButton";
 export default function Dashboard() {
   const router = useRouter();
   const { data: session, status } = useSession();
@@ -28,9 +28,39 @@ export default function Dashboard() {
   }, [status]);
 
   console.log(dataState);
-  return (
-    <div>
-      dashboard <br /> <br /> <AuthButton />
-    </div>
-  );
+  if (status === "loading") {
+    return "Loading or not authenticated...";
+  } else {
+    return (
+      <>
+        {dataState == null && (
+          <>
+            <div className="max-w-[90%] sm:w-72 md:w-96 mx-auto text-center flex flex-col justify-center items-center gap-4 my-6">
+              {" "}
+              You are not logged in <AuthButton />
+              <p>
+                New to Platform?{" "}
+                <Link href="/register" className="text-sky-600">
+                  Register here
+                </Link>
+              </p>
+            </div>
+          </>
+        )}
+        {dataState == 0 && (
+          <>
+            Sorry Your account is not registered as a doctor{" "}
+            <Link href="/register" className="text-sky-600">
+              register it here
+            </Link>
+          </>
+        )}
+        {dataState != 0 && dataState != null && (
+          <>
+            {JSON.stringify(dataState)} <AuthButton />
+          </>
+        )}
+      </>
+    );
+  }
 }
