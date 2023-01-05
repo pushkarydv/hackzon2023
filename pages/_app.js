23f7513d9a34c3daf39a78d19c1c7fd33a8c0af2
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2023-01-05 14:29:16 +0530

    tailwind config

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
-import '../styles/globals.css'
+import "../styles/tailwind_config.css";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className="custom-font min-h-screen w-full bg-slate-50 text-slate-900">
+      <Component {...pageProps} />
+    </main>
+  );
 }
