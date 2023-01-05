0dd1afedb5337fb06c6392e818234b2b633f7602
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2023-01-06 04:08:15 +0530

    doctor dashboard

--- pages/doctor/dashboard.js
+++ pages/doctor/dashboard.js
@@ -1,27 +1,36 @@
+import { doc, getDoc } from "firebase/firestore";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AuthButton from "../../components/AuthenticationButtons/AuthButton";
+import { db } from "../../config/firebase";
 
 export default function Dashboard() {
   const router = useRouter();
-  const { data, status } = useSession();
-  const [loggedIn, setLoggedIn] = useState(false);
+  const { data: session, status } = useSession();
+  const [dataState, setDataState] = useState(null);
+  useEffect(() => {
+    if (status === "authenticated") {
+      async function fetchPatientsDetails() {
+        const docRef = doc(db, "doctors", session.user.email);
+        const docSnap = await getDoc(docRef);
+        const dbdata = docSnap.data();
+        if (docSnap.exists()) {
+          setDataState(dbdata);
+        } else {
+          // when no data of user set this state 0 so that we can check
+          setDataState(0);
+        }
+      }
 
-  if (status === "loading") {
-    return "Loading or not authenticated...";
-  }
-  if (status === "authenticated") {
-    return (
-      <div>
-        dashboard <br /> {JSON.stringify(data)} <br /> <AuthButton />
-      </div>
-    );
-  } else {
-    return (
-      <>
-        <AuthButton />
-      </>
-    );
-  }
+      fetchPatientsDetails();
+    }
+  }, [status]);
+
+  console.log(dataState);
+  return (
+    <div>
+      dashboard <br /> <br /> <AuthButton />
+    </div>
+  );
 }
