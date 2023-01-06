c35d4d7a2f66d1bf5bcb6d3b37f6f6b245b9e512
AniruddhaSoni <anirudh.soni007@gmail.com>
2023-01-06 06:57:36 +0530

    find page

--- pages/find.jsx
+++ pages/find.jsx
@@ -33,9 +33,23 @@ export default function find() {
           </button>
         </form>
         <div
-          className="grid grid-cols-1 md:grid-cols-2 lg:grid-col-3 xl:grid-cols-4"
+          className="grid grid-cols-1 md:grid-cols-2 lg:grid-col-3 xl:grid-cols-4 gap-4 py-8"
           id="results"
-        ></div>
+        >
+          <div className="ring rounded-3xl flex flex-col items-center p-2">
+            <img
+              src="https://lh3.googleusercontent.com/a/AEdFTp6lbEqUQdLWw42mDUg4q0zbd1L7HJEUHAar_OFZ"
+              alt=""
+              srcset=""
+              className="ring-2 rounded w-10/12 object-cover h-10/12"
+            />
+            <div className="text-lg font-bold flex flex-col gap-2 items-center">
+              Dr. Ashish Kumar (MBBS)
+              <div className="badge badge-info badge-lg">Cardiologist</div>
+              Over 7 Years Experiance
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
